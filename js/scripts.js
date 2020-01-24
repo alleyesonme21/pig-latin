d7e610d610a6be7de81d21f2572b531a8ab41433
Johnny Duverseau and Sommer Howe <johnny@>
2020-01-23 16:33:44 -0800

    add ay at the end of a word starting wsith a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@ $(document).ready(function() {
     event.preventDefault();
     var words = $("input#sentence").val();
     var sentarray = words.split("");
-    var vowels = ["a", "e", "i", "o", "u"]
+    var vowels = ["a", "e", "i", "o", "u"]; 
 
     console.log(sentarray);
   
-    for (var index =0; index < vowels.length; index+= 1){
-    if (sentarray[0] === vowels[index]) {
-        alert("true");
+    // for (var index =0; index < vowels.length; index+= 1){
+    if (sentarray[0] === vowels[0] || vowels[1] || vowels[2] || vowels[3]) {
+        console.log(sentarray.slice(1) + "ay");
     } else {
-      
+      alert("false");
     }
-    }
+    // }
   
 
 
